6281da615972c42b2d05fb36b90c63aa3b8483a4
j-ke614 <corn614@gmail.com>
2022-05-14 16:21:59 -0600

    week 4 notes

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const links = [
     {
         label: "Week3 Notes",
         url: "week3/index.html"
+    },
+    {
+        label: "Week4 Notes",
+        url: "week4/index.html"
     }
     ]
 
