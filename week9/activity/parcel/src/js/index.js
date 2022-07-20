efea6eef4a809bb664d92e4d0e35a385841a679e
j-ke614 <corn614@gmail.com>
2022-07-19 22:10:58 -0600

    portfolio update

---
+++ week9/activity/parcel/src/js/index.js
@@ -0,0 +1,15 @@
+import shipController from "./ships";
+
+console.log(shipController.getShips());
+console.log(shipController.getShipByName("Executor"));
+
+// check to see if javascript is working
+function component() {
+  const element = document.createElement("p");
+
+  element.innerHTML = "Setup Appears to be working &#128521;";
+
+  return element;
+}
+
+document.body.appendChild(component());
